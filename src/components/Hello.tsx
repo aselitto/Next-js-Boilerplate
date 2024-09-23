c5168e2a32531781f6666829cba6ba2261590aa5
aselitto <aselitto@gmail.com>
2024-09-23 19:53:50 +0000

    style((pages)): updated more page documentation
    updated the dashboard for click here

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -12,7 +12,7 @@ const Hello = async () => {
         {t('hello_message', { email: user?.emailAddresses[0]?.emailAddress })}
       </p>
       <p>
-        {`Looking for a SaaS Boilerplate? `}
+        `Lets scan your insurance cards!`
         <a
           className="text-blue-700 hover:border-b-2 hover:border-blue-700"
           href="https://google.com"
