d6b851a472133064fb5e29481f8b7950533705ed
aselitto <aselitto@gmail.com>
2024-09-26 01:59:17 +0000

    feat((css)): update mobile auth

--- src/app/[locale]/(auth)/dashboard/user-profile/[[...user-profile]]/page.tsx
+++ src/app/[locale]/(auth)/dashboard/user-profile/[[...user-profile]]/page.tsx
@@ -15,7 +15,7 @@ export async function generateMetadata(props: { params: { locale: string } }) {
 }
 
 const UserProfilePage = (props: { params: { locale: string } }) => (
-  <div className="mx-auto my-6 max-w-md px-0">
+  <div className="mx-auto my-6 max-w-md px-2">
     <UserProfile
       path={getI18nPath('/dashboard/user-profile', props.params.locale)}
     />
