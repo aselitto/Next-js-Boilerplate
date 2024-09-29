f71586071ff4146bbda207b839aea5124519f3ac
aselitto <aselitto@gmail.com>
2024-09-29 03:28:14 +0000

    style((css)): fixed some alignment issues with user profile

--- src/app/[locale]/(auth)/dashboard/user-profile/[[...user-profile]]/page.tsx
+++ src/app/[locale]/(auth)/dashboard/user-profile/[[...user-profile]]/page.tsx
@@ -15,7 +15,7 @@ export async function generateMetadata(props: { params: { locale: string } }) {
 }
 
 const UserProfilePage = (props: { params: { locale: string } }) => (
-  <div className="mx-auto my-6 max-w-md px-2">
+  <div className="mx-auto my-6 w-full max-w-md px-2 md:max-w-lg md:px-4 lg:max-w-xl">
     <UserProfile
       path={getI18nPath('/dashboard/user-profile', props.params.locale)}
     />
